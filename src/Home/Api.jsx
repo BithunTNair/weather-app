55648b382a704323504c08dbe42d6fb17b510c5e
BithunT-Nair <react666cr@gmail.com>
2024-11-28 16:23:44 +0530

    second commit

--- src/Home/Api.jsx
+++ src/Home/Api.jsx
@@ -3,10 +3,16 @@ import React, { useEffect, useState } from 'react'
 
 const Api = () => {
     const [weather, setWeather] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    var city = 'varkala'
+    // const getcity = (name) => {
+    //     var city = name
+    // }
     useEffect(() => {
+        // getcity('varkala')
         axios({
             method: 'GET',
-            url: 'https://api.openweathermap.org/data/2.5/weather?q=varkala&appid={}'
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ import.meta.env.VITE_API_KEY}`
 
         }).then((response) => {
             console.log(response);
@@ -16,10 +22,24 @@ const Api = () => {
             console.log(error);
 
         })
-    },[])
+    }, []);
+    const getName = (e) => {
+        console.log(e.target.value);
+        setInputValue(e.target.value)
+
+    };
+    const giveCity = (name) => {
+       
+
+    }
     return (
         <div>
-
+            <div className='w-full h-screen bg-zinc-900 flex justify-center items-center'>
+                <div className='w-1/2 h-3/4 bg-neutral-600 flex  items-start justify-center '>
+                    <input type="text" className='shadow appearance-none border rounded mt-4 w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='Type your city' onChange={getName} value={inputValue} />
+                    <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 ml-4' onClick={() => giveCity(inputValue)} > search </button>
+                </div>
+            </div>
         </div>
     )
 }
